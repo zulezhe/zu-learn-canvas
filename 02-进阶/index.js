90f1a7c13cc83efb8e353cfacf407a7c89106cd9
Oliver <wang59651@foxmail.com>
2021-01-29 16:43:09 +0800

    feat:add 110000geojson

---
+++ 02-进阶/index.js
@@ -0,0 +1,26 @@
+/*
+ * @Author: zulezhe
+ * @Date: 2021-01-29 16:22:41
+ * @LastEditors: zulezhe
+ * @LastEditTime: 2021-01-29 16:33:15
+ * @Description: In User Settings Edit
+ * @FilePath: \canvas\02-进阶\index.js
+ */
+class MyCanvas {
+  constructor(el, width, height) {
+    this.el = doeumeng.getElementById(el);
+    this.width = width;
+    this.height = height;
+    this.initCanvas();
+  }
+  initCanvas() {
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+    this.ctx = this.canvas.getContext("2d");
+    this.el.appendChild(this.canvas);
+  }
+  getContext() {
+    return this.ctx;
+  }
+}
