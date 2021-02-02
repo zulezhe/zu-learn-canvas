13917cdb72a60d439626cf7d13c4de3da9e93694
Oliver <wang59651@foxmail.com>
2021-02-02 18:13:26 +0800

    更新进阶

--- 02-进阶/index.js
+++ 02-进阶/index.js
@@ -2,7 +2,7 @@
  * @Author: zulezhe
  * @Date: 2021-01-29 16:22:41
  * @LastEditors: zulezhe
- * @LastEditTime: 2021-01-29 16:48:11
+ * @LastEditTime: 2021-02-02 15:48:55
  * @Description: In User Settings Edit
  * @FilePath: \canvas\02-进阶\index.js
  */
@@ -11,7 +11,10 @@ class MyCanvas {
     this.el = document.getElementById(el);
     this.width = width;
     this.height = height;
+    this.timer = null;
     this.initCanvas();
+    this.drawBg();
+    this.resizeChange();
   }
   initCanvas() {
     this.canvas = document.createElement("canvas");
@@ -19,6 +22,26 @@ class MyCanvas {
     this.canvas.height = this.height;
     this.ctx = this.canvas.getContext("2d");
     this.el.appendChild(this.canvas);
+  }
+  drawBg() {
+    const img = new Image();
+    img.src = "./images/bg.png";
+    img.onload = () => {
+      const pattern = this.ctx.createPattern(img, "repeat");
+      this.ctx.fillStyle = pattern;
+      console.log(pattern, this.width, this.height);
+      this.ctx.fillRect(0, 0, this.width, this.height);
+    };
+  }
+  resizeChange() {
+    let count = 1;
+    window.addEventListener("resize", () => {
+      this.timer ? window.clearTimeout(this.timer) : "";
+      this.timer = setTimeout(() => {
+        this.initCanvas();
+        this.drawBg();
+      }, 500);
+    });
   }
   getContext() {
     return this.ctx;
